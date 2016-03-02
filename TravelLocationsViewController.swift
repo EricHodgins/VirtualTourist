4a1c905bedde4186c5955ac0cc14b4e5269ce978
Eric Hodgins <hodgins.e@gmail.com>
2016-03-01 22:51:49 -0500

    Initial commit.

---
+++ TravelLocationsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TravelLocationsViewController.swift
+//  VirtualTourist
+//
+//  Created by Eric Hodgins on 2016-03-01.
+//  Copyright © 2016 Eric Hodgins. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationsViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
---
+++ VirtualTourist/PhotoAlbumViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Eric Hodgins on 2016-03-01.
+//  Copyright © 2016 Eric Hodgins. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func newCollectionButtonPressed(sender: AnyObject) {
+        
+    }
+
+}
